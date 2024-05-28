98b14584b3ec32b38395e0f1a200641a03d478e0
JedrzejPolarczyk <115351061+JedrzejPolarczyk@users.noreply.github.com>
2024-05-28 12:26:38 +0200

    Add files via upload

---
+++ DataBaseModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsArchive
+{
+    public class BookshelfMain
+    {
+        public int Id { get; set; }
+        public string Stand { get; set; }
+        public string Shelf { get; set; }
+        public string MainItemName { get; set; }
+        public List<BookshelfSub> SubItems { get; set; }
+    }
+
+    public class BookshelfSub
+    {
+        public int Id { get; set; }
+        public string SubItemName { get; set; }
+        public bool IsTaken { get; set; }
+        public string LoginName { get; set; }
+        public int BookshelfMainId { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace YourNamespace
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            DataService dataService = new DataService();
+            var data = dataService.GetBookshelfData();
+            BookshelfGrid.ItemsSource = data;
+        }
+    }
+}
+
